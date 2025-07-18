cc050a3b8428a87c87ecb63d6efd757be0c9e6a6
mariawanjiru <rushi.mariah@gmail.com>
2025-07-18 13:40:16 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Step 1: Remove the <main> element
+const main = document.querySelector('main');
+main.remove();
+
+// Step 2: Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Step 3: Set the id
+newHeader.id = 'victory';
+
+// Step 4: Set the text content
+newHeader.textContent = 'Mary Wanjiru is the champion';
+
+// Step 5: Append to body
+document.body.append(newHeader);// Write your code here!
